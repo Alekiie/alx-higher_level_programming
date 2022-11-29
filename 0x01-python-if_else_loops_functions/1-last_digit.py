b06a650d125a9ecdf7ff4abd3a9bc66d539552fd
Alexander <amnjogu78@gmail.com>
2022-11-29 03:59:15 -0800

    Update task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ string = "Last digit of"
 if number < 0:
     x = number % -10
 elif number >= 0:
-   x = number % 10
+    x = number % 10
 if(x > 5):
     print(f"{string} {number} is {x} and is greater than 5")
 elif(x == 0):
